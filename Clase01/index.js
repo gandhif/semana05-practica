4cc0be93230e4ebf574f3cd1a601ad05088e69e6
gandhi <gandhiflores@gmail.com>
2022-03-18 22:00:59 -0500

    calculadora

--- Clase01/index.js
+++ Clase01/index.js
@@ -19,4 +19,37 @@ resultado.textContent=3;
 
 }
 function Calculadora(){
+    
+    let var1 = parseInt(prompt("Ingrese Valor 1"));
+    let ope = parseInt(prompt("ingres una opcion: 1 suma / 2 resta /3 multiplicacion /4 division"));    
+    let var2 = parseInt(prompt("Ingrese Valor 2"));
+    let pro=0;
+    switch (ope) {
+        case 1:
+            pro =(var1)+(var2);
+            alert("Resultado: " + pro);
+            break;
+
+        case 2:
+            pro =(var1)-(var2);
+            alert("Resultado: " + pro);
+            break;
+        case 3:
+            pro =(var1)*(var2);
+            alert("Resultado: " + pro);
+            break;
+    
+        case 4:
+            if (var2==0) {
+                alert("No definido");
+            } else {
+            pro =var1 / var2;
+            alert("Resultado: " + pro);
+            }            
+            break;
+    
+        default:
+            alert("Opcion no valido");
+            break;
+    }
 }
